30be1a85681b7e66f06f29b29cfcbed9e49dd83c
Simon Monsalves <simonsalves@gmail.com>
2014-10-05 14:51:04 -0300

    login funcionando

--- valdivia-travel/SQL-DB.sql
+++ valdivia-travel/SQL-DB.sql
@@ -1,45 +1,74 @@
 
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'pyme'
+-- 
+-- ---
+
 DROP TABLE IF EXISTS `pyme`;
-		
+    
 CREATE TABLE `pyme` (
   `id_pyme` SMALLINT NOT NULL AUTO_INCREMENT,
   `descripcion` VARCHAR(50) NOT NULL,
-  `telefono` VARCHAR(10) DEFAULT NULL,
+  `telefono` VARCHAR(10) NULL DEFAULT NULL,
   `horario` VARCHAR(10) NOT NULL,
   `direccion` VARCHAR(50) NOT NULL,
-  `url` VARCHAR(20) DEFAULT NULL,
-  `correo` VARCHAR(25) DEFAULT NULL,
+  `latitud` VARCHAR(10) NOT NULL,
+  `longitud` VARCHAR(10) NOT NULL,
+  `url` VARCHAR(20) NULL DEFAULT NULL,
+  `correo` VARCHAR(25) NULL DEFAULT NULL,
   `id_tipo` SMALLINT NOT NULL,
   PRIMARY KEY (`id_pyme`)
 );
 
+-- ---
+-- Table 'dueño_pyme'
+-- 
+-- ---
 
-DROP TABLE IF EXISTS `dueno_pyme`;
-		
-CREATE TABLE `dueno_pyme` (
-  `id_dueno_pyme` SMALLINT AUTO_INCREMENT NOT NULL,
+DROP TABLE IF EXISTS `dueño_pyme`;
+    
+CREATE TABLE `dueño_pyme` (
+  `id_dueño_pyme` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `id_cliente_pyme` VARCHAR(25) NOT NULL,
   `id_pyme` SMALLINT NOT NULL,
   `id_user` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id_dueno_pyme`)
+  PRIMARY KEY (`id_dueño_pyme`)
 );
 
+-- ---
+-- Table 'cliente_turismo'
+-- 
+-- ---
 
 DROP TABLE IF EXISTS `cliente_turismo`;
-		
+    
 CREATE TABLE `cliente_turismo` (
-  `id_cliente_turismo` VARCHAR(50),
+  `id_cliente_turismo` VARCHAR(50) NOT NULL,
   `nombre` VARCHAR(15) NOT NULL,
   `passwd` VARCHAR(15) NOT NULL,
   PRIMARY KEY (`id_cliente_turismo`)
 );
 
+-- ---
+-- Table 'lugares_visitados'
+-- 
+-- ---
 
 DROP TABLE IF EXISTS `lugares_visitados`;
-		
+    
 CREATE TABLE `lugares_visitados` (
-  `id_visita_lugar` INTEGER NOT NULL AUTO_INCREMENT,
-  `id_lugar` SMALLINT NOT NULL,
+  `id_visita_lugar` SMALLINT NOT NULL AUTO_INCREMENT,
+  `id_lugar` SMALLINT NOT NULL DEFAULT NULL,
   `id_cliente_turismo` VARCHAR(50) NOT NULL,
   `fecha` DATE NOT NULL,
   PRIMARY KEY (`id_visita_lugar`)
@@ -51,26 +80,32 @@ CREATE TABLE `lugares_visitados` (
 -- ---
 
 DROP TABLE IF EXISTS `pyme_visitada`;
-		
+    
 CREATE TABLE `pyme_visitada` (
-  `id_pyme_visitada` INTEGER AUTO_INCREMENT,
+  `id_pyme_visitada` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `id_cliente_turismo` VARCHAR(50) NOT NULL,
   `id_pyme` SMALLINT NOT NULL,
-  `fecha` DATE NULL,
+  `fecha` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`id_pyme_visitada`)
 );
 
+-- ---
+-- Table 'lugar'
+-- 
+-- ---
 
 DROP TABLE IF EXISTS `lugar`;
-		
+    
 CREATE TABLE `lugar` (
-  `id_lugar` SMALLINT NOT NULL AUTO_INCREMENT,
+  `id_lugar` SMALLINT NOT NULL AUTO_INCREMENT DEFAULT NULL,
   `descripcion` VARCHAR(50) NOT NULL,
   `direccion` VARCHAR(50) NOT NULL,
-  `telefono` VARCHAR(10) DEFAULT NULL,
+  `telefono` VARCHAR(10) NULL DEFAULT NULL,
   `horario` VARCHAR(15) NOT NULL,
-  `url` VARCHAR(50) DEFAULT NULL,
-  `correo` VARCHAR(50) DEFAULT NULL,
+  `latitud` VARCHAR(10) NULL DEFAULT NULL,
+  `longitud` VARCHAR(10) NOT NULL,
+  `url` VARCHAR(50) NULL DEFAULT NULL,
+  `correo` VARCHAR(50) NOT NULL DEFAULT 'NULL',
   PRIMARY KEY (`id_lugar`)
 );
 
@@ -80,27 +115,36 @@ CREATE TABLE `lugar` (
 -- ---
 
 DROP TABLE IF EXISTS `user`;
-		
+    
 CREATE TABLE `user` (
   `id_user` VARCHAR(25) NOT NULL,
-  `nombre` VARCHAR(15) NOT NULL,
+  `nombre` VARCHAR(15) NULL DEFAULT NULL,
   `apellido_p` VARCHAR(15) NOT NULL,
   `apellido_m` VARCHAR(15) NOT NULL,
   `passwd` VARCHAR(15) NOT NULL,
   PRIMARY KEY (`id_user`)
 );
 
+-- ---
+-- Table 'tipo_pyme'
+-- 
+-- ---
 
 DROP TABLE IF EXISTS `tipo_pyme`;
-		
+    
 CREATE TABLE `tipo_pyme` (
   `id_tipo` SMALLINT NOT NULL AUTO_INCREMENT,
   `descripcion` VARCHAR(15) NOT NULL,
   PRIMARY KEY (`id_tipo`)
 );
 
+-- ---
+-- Table 'cliente_pyme'
+-- 
+-- ---
+
 DROP TABLE IF EXISTS `cliente_pyme`;
-		
+    
 CREATE TABLE `cliente_pyme` (
   `id_cliente_pyme` VARCHAR(25) NOT NULL,
   `nombre` VARCHAR(15) NOT NULL,
@@ -112,21 +156,34 @@ CREATE TABLE `cliente_pyme` (
 );
 
 -- ---
--- FOREIGN KEY
+-- Foreign Keys 
 -- ---
 
 ALTER TABLE `pyme` ADD FOREIGN KEY (id_tipo) REFERENCES `tipo_pyme` (`id_tipo`);
-ALTER TABLE `dueno_pyme` ADD FOREIGN KEY (id_cliente_pyme) REFERENCES `cliente_pyme` (`id_cliente_pyme`);
-ALTER TABLE `dueno_pyme` ADD FOREIGN KEY (id_pyme) REFERENCES `pyme` (`id_pyme`);
-ALTER TABLE `dueno_pyme` ADD FOREIGN KEY (id_user) REFERENCES `user` (`id_user`);
+ALTER TABLE `dueño_pyme` ADD FOREIGN KEY (id_cliente_pyme) REFERENCES `cliente_pyme` (`id_cliente_pyme`);
+ALTER TABLE `dueño_pyme` ADD FOREIGN KEY (id_pyme) REFERENCES `pyme` (`id_pyme`);
+ALTER TABLE `dueño_pyme` ADD FOREIGN KEY (id_user) REFERENCES `user` (`id_user`);
 ALTER TABLE `lugares_visitados` ADD FOREIGN KEY (id_lugar) REFERENCES `lugar` (`id_lugar`);
 ALTER TABLE `lugares_visitados` ADD FOREIGN KEY (id_cliente_turismo) REFERENCES `cliente_turismo` (`id_cliente_turismo`);
 ALTER TABLE `pyme_visitada` ADD FOREIGN KEY (id_cliente_turismo) REFERENCES `cliente_turismo` (`id_cliente_turismo`);
 ALTER TABLE `pyme_visitada` ADD FOREIGN KEY (id_pyme) REFERENCES `pyme` (`id_pyme`);
 
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `pyme` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `dueño_pyme` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `cliente_turismo` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `lugares_visitados` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `pyme_visitada` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `lugar` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `user` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `tipo_pyme` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `cliente_pyme` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
---	INSERT EN CADA UNA DE LAS TABLAS
+-- Test Data
 -- ---
 
 -- INSERT INTO `pyme` (`id_pyme`,`descripcion`,`telefono`,`horario`,`direccion`,`latitud`,`longitud`,`url`,`correo`,`id_tipo`) VALUES
@@ -147,3 +204,4 @@ ALTER TABLE `pyme_visitada` ADD FOREIGN KEY (id_pyme) REFERENCES `pyme` (`id_pym
 -- ('','');
 -- INSERT INTO `cliente_pyme` (`id_cliente_pyme`,`nombre`,`apellido_p`,`apellido_m`,`rut`,`passwd`) VALUES
 -- ('','','','','','');
+
